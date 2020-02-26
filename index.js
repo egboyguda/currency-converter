e6b73de62fd9e16c78ce0ad80580bc6e564f178f
egboyguda <e.gboyguda@gmail.com>
2020-02-26 09:37:23 +0800

    connect to api and get response

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const request = require('request');
 var bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: true });
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/",function(req,res){
-	res.sendFile(__dirname+"index.html")
+	res.sendFile(__dirname+"/index.html")
+})
+app.post("/",function(req,res){
+	let base = req.body.base.toUpperCase();
+	let flat = req.body.flat.toUpperCase();
+	console.log(base+","+flat)
+	var options ={
+		url:"https://api.exchangeratesapi.io/latest",
+		method:"GET",
+		qs:{
+			symbols:base+","+flat
+		}
+	}
+	request(options,function(error,response,body){
+		var data=JSON.parse(body);
+		console.log(data);
+	})
+
 })
 
 
